2a3db12146f8de1d95ec0f8e641ea23b9d716591
Chung <pirate1912@gmail.com>
2019-01-24 19:27:07 +0900

    git status

--- app/src/main/java/ntc/dev4u/com/myapplication/MainActivity.java
+++ app/src/main/java/ntc/dev4u/com/myapplication/MainActivity.java
@@ -30,59 +30,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //String request Method.GET
-        volleyStringRequest("2DEV4U.COM", "contact@2dev4u.com", "http://2dev4u.com/wp-content/uploads/2016/12/cropped-2dev4u.comNotes-of-Developer-1.png");
 
-        //JsonObject request Method.GET
-        volleyJsonObjectRequest();
+        Map<String, String> postParam= new HashMap<String, String>();
+        postParam.put("login_id", "chungtv");
+        postParam.put("password", "123456");
 
-        //JsonArray request Method.GET
-        volleyJsonArrayRequest();
-
-        //Image request
-        volleyImageRequest();
+        volleyJsonObjectRequest(new JSONObject(postParam));
     }
 
-    private void volleyImageRequest() {
-        ImageRequest imageRequest = new ImageRequest("http://2dev4u.com/wp-content/uploads/2016/10/take-a-selfie-with-js.png", new Response.Listener<Bitmap>() {
-            @Override
-            public void onResponse(Bitmap response) {
-                Log.e(TAG, "ImageRequest onResponse: " + response.toString());
-            }
-        }, 0, 0, null, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "ImageRequest onErrorResponse: " + error.getMessage());
-            }
-        });
-        VolleySingleton.getInstance(this).getRequestQueue().add(imageRequest);
-    }
 
-    private void volleyJsonArrayRequest() {
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest("http://dev.2dev4u.com/news/api.php", new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                Log.e(TAG, "JsonArrayRequest onResponse: " + response.toString());
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "JsonArrayRequest onErrorResponse: " + error.getMessage());
-            }
-        });
-        VolleySingleton.getInstance(this).getRequestQueue().add(jsonArrayRequest);
-    }
-
-    private void volleyJsonObjectRequest() {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://api.ipify.org/?format=json", null,
+    private void volleyJsonObjectRequest(JSONObject object) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://157.7.165.192/member/login", object,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        try {
-                            Log.e(TAG, "JsonObjectRequest onResponse: " + response.getString("ip").toString());
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        Log.d(TAG, "JsonObjectRequest onResponse: " + response.toString());
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -93,53 +55,4 @@ public class MainActivity extends AppCompatActivity {
         VolleySingleton.getInstance(this).getRequestQueue().add(jsonObjectRequest);
     }
 
-    private void volleyStringRequest(final String name, final String email, final String avatar) {
-        StringRequest stringRequest = new StringRequest(Request.Method.GET,
-                "http://dev.2dev4u.com/news/api.php?email=" + email,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e(TAG, "StringRequest onResponse: " + response.toString());
-                        if (!response.equals("User doesn't exist")) {
-                            // When user exists will return id_user
-                            Log.e(TAG, "StringRequest onResponse: User is exists: id= " + response);
-                        } else {
-                            volleyPostStringRequest(name, email, avatar);
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Volley", error.getMessage());
-            }
-        });
-
-        VolleySingleton.getInstance(this).getRequestQueue().add(stringRequest);
-    }
-
-    //String request Method.POST
-    private void volleyPostStringRequest(final String name, final String email, final String avatar) {
-        StringRequest insertRequest = new StringRequest(Request.Method.POST, "http://dev.2dev4u.com/news/api.php", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.e(TAG, "StringRequest Post" + response.toString());
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, error.toString());
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email", email);
-                params.put("name", name);
-                params.put("avatar", avatar);
-
-                return params;
-            }
-        };
-        VolleySingleton.getInstance(this).getRequestQueue().add(insertRequest);
-    }
 }
